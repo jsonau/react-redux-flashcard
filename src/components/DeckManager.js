14f31f7d94acfa2cad9c9657e9a75d277f430c1f
Jason Au <jsonau@gmail.com>
2016-10-17 17:12:18 -0400

    DeckManager input and submit ui in progress

--- src/components/DeckManager.js
+++ src/components/DeckManager.js
@@ -1,9 +1,29 @@
 import React, { PropTypes } from 'react'
 
 class DeckManager extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {title:''};
+	}
+
+	onNewDeckSubmit(e){
+		e.preventDefault();
+		if (this.state.title === "") return;
+		console.log(this.state.title)
+
+	}	
+	onInputChange(e){
+	    this.setState({title: e.target.value});
+	}
+
 	render(){
 		return(
 			<div className="deckManager">
+				<form onSubmit={this.onNewDeckSubmit.bind(this)}>
+					<input type="text" placeholder="Give a title for a newdeck" onChange={this.onInputChange.bind(this)} />
+					<input type="submit"/>
+				</form>
+
 				<ul className="deckList">
 					{this.props.decks.map(deck =>
 						<li key={deck.id} onClick={()=>this.props.onSelectDeck(deck.id)}>{deck.title}</li>
